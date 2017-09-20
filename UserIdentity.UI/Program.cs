0219944daf06f9fe250a90cc7ed9ee96f01e15a2
cleberdsantos <systemofcleber@gmail.com>
2017-09-20 20:42:00 -0300

    ignore sonnar

--- UserIdentity.UI/Program.cs
+++ UserIdentity.UI/Program.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace UserIdentity.UI
 {
-    public class Program
+    public class Program //NOSONAR
     {
         public static void Main(string[] args)
         {
