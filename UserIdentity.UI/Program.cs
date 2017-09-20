abd220b545fad695aa7da3cd4e7d6c68a42d721b
cleberdsantos <systemofcleber@gmail.com>
2017-09-20 19:04:20 -0300

    teste

---
+++ UserIdentity.UI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace UserIdentity.Api
+{
+    public class Program //NOSONAR
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
